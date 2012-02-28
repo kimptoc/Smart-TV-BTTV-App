1896b2fa23330f37e8c609ce35cdef2d6db05861
Chris Kimpton <chris@kimptoc.net>
2012-02-28 20:40:39 +0000

    now with sprockets/minify working :)

---
+++ source/javascripts/application.js
@@ -0,0 +1,27 @@
+//= require jquery-1.7.1
+//= require serenade
+//= require keyboard
+//= require app_init
+//= require app/config_loader
+//= require app/app_ready
+//= require app/load_views
+//= require app/setup_keymapping
+//= require app/station_controller
+//= require config
+//= require samsung
+//= require app
+
+
+//<script defer src="js/serenade.js"></script>
+//<script defer src="js/jquery-1.7.1.js"></script>
+//<script defer src="js/keyboard.js"></script>
+//<script defer src="js/app.js"></script>
+//<script defer src="js/config.js"></script>
+//<script defer src="js/samsung.js"></script>
+//<script defer src="js/app/setup_keymapping.js"></script>
+//<script defer src="js/app/station_controller.js"></script>
+//<script defer src="js/app/config_loader.js"></script>
+//<script defer src="js/app/app_ready.js"></script>
+//<script defer src="js/app/load_views.js"></script>
+
+// = r e quire_tree .
