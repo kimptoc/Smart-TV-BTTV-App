02a6d5027b16ee8df2d43b1961e8f3d9e148e40e
Chris Kimpton <chris@kimptoc.net>
2012-02-29 22:22:45 +0000

    move stuff to remote, make viewer look like sample

--- source/javascripts/app/app_ready.js.coffee
+++ source/javascripts/app/app_ready.js.coffee
@@ -1,6 +1,7 @@
 bttv.AppReady = ->
   bttv.log "App Ready!"
-  document.getElementById("app_stylesheet").href = "#{bttv.station.get("content_url_root")}/css/app.css"
+  document.getElementById("app_stylesheet1").href = "#{bttv.station.get("content_url_root")}/css/Main.css"
+  document.getElementById("app_stylesheet2").href = "#{bttv.station.get("content_url_root")}/css/app.css"
   bttv.station_controller = new bttv.StationController()
   bttv.setupKeymapping()
   bttv.station_controller.handleShowStations()
--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -12,16 +12,3 @@
 //= require app
 
 
-//<script defer src="js/serenade.js"></script>
-//<script defer src="js/jquery-1.7.1.js"></script>
-//<script defer src="js/keyboard.js"></script>
-//<script defer src="js/app.js"></script>
-//<script defer src="js/config.js"></script>
-//<script defer src="js/samsung.js"></script>
-//<script defer src="js/app/setup_keymapping.js"></script>
-//<script defer src="js/app/station_controller.js"></script>
-//<script defer src="js/app/config_loader.js"></script>
-//<script defer src="js/app/app_ready.js"></script>
-//<script defer src="js/app/load_views.js"></script>
-
-// = r e quire_tree .
